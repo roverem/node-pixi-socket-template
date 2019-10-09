41a97fc2edde5a7bd0cdeaf7492af1d31d32cfe3
Martin Rovere <martinr@qb9.net>
2019-10-09 14:11:29 -0300

    fix carpetas y node_modules borrado

---
+++ server/main.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import http from 'http';
+import io from 'socket.io';
+
+
+const APP = express();
+const HTTP = http.createServer(APP);
+const IO = io(HTTP);
+
+APP.use('/', express.static("./client"));
+
+console.log(APP);
+
+
+IO.on('connection', function(socket){
+	console.log("user connected");
+	
+	socket.on('disconnect', function(){
+		console.log("user disconnected");
+	});
+});
+
+HTTP.listen(process.env.PORT || 3000, function(){
+	console.log('listening on *:3000');
+});
